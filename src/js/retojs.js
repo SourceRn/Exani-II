7f2c34b782d559bcbeae3e2a4ddb254e4cc0ca72
SourceRn <ramnievesg@gmail.com>
2021-10-18 15:19:43 -0500

    formulario

--- src/js/retojs.js
+++ src/js/retojs.js
@@ -9,54 +9,62 @@ class Preguntas{
 
 //Preguntas
 pregunta1 = new Preguntas(
-        'Aqui va la pregunta1',
-        ['Respuesta1', 'Respuesta2', 'Respuesta3', 'Respuesta4'],
-        'RespuestaCorrecta'
+    '3 {1 + 2 [ 4 + 5 (6 - 7) + 8] - 9} =',
+    ['a. -18', 'b. 18', 'c. 16', 'd. 13'],
+    'b'
 );
 pregunta2 = new Preguntas(
-    'Aqui va la pregunta2',
-    ['Respuesta1', 'Respuesta2', 'Respuesta3', 'Respuesta4'],
-    'RespuestaCorrecta'
+    '3(4-5)2 + 4(5-1) - 3(1+2)2(2) + 2(3)(3) =',
+    ['a. -26', 'b. -17', 'c. -94', 'd. 26'],
+    'b'
 );
 pregunta3 = new Preguntas(
-    'Aqui va la pregunta3',
-    ['Respuesta1', 'Respuesta2', 'Respuesta3', 'Respuesta4'],
-    'RespuestaCorrecta'
+    '4 [ (5 + 3) - 22] =',
+    ['a. -56', 'b. 10', 'c. 17', 'd. 18'],
+    'a'
 );
 pregunta4 = new Preguntas(
-    'Aqui va la pregunta4',
-    ['Respuesta1', 'Respuesta2', 'Respuesta3', 'Respuesta4'],
-    'RespuestaCorrecta'
+    '150 + (25 * 2) + 32 / (8*2) =',
+    ['a. 208', 'b. 202', 'c. 340', 'd. 58'],
+    'b'
 );
 pregunta5 = new Preguntas(
-    'Aqui va la pregunta5',
-    ['Respuesta1', 'Respuesta2', 'Respuesta3', 'Respuesta4'],
-    'RespuestaCorrecta'
+    '(30 – 20) / 2 + (6*5) / 3 + (40 – 25) + (9-6) =',
+    ['a. 20', 'b. 33', 'c. 30', 'd. 21'],
+    'b'
 );
+imgPregunt6 = document.createElement("img");
+imgPregunt6.src = "src/images/reto/ejercicios/eje6.PNG";
 pregunta6 = new Preguntas(
-    'Aqui va la pregunta6',
-    ['Respuesta1', 'Respuesta2', 'Respuesta3', 'Respuesta4'],
-    'RespuestaCorrecta'
+    imgPregunt6,
+    ['a. 13', 'b. 10', 'c. -9', 'd. 9'],
+    'c'
 );
+imgPregunt7 = document.createElement("img");
+imgPregunt7.src = "src/images/reto/ejercicios/eje7.jpeg";
 pregunta7 = new Preguntas(
-    'Aqui va la pregunta7',
-    ['Respuesta1', 'Respuesta2', 'Respuesta3', 'Respuesta4'],
-    'RespuestaCorrecta'
+    imgPregunt7,
+    ['a. 7/14', 'b. 5/16', 'c. 6/14', 'd. 7/16'],
+    'd'
 );
+imgPregunt8 = document.createElement("img");
+imgPregunt8.src = "src/images/reto/ejercicios/eje8.jpeg";
 pregunta8 = new Preguntas(
-    'Aqui va la pregunta8',
-    ['Respuesta1', 'Respuesta2', 'Respuesta3', 'Respuesta4'],
-    'RespuestaCorrecta'
+    imgPregunt8,
+    ['a. 2/7', 'b. 3.5', 'c. -1.8', 'd. -7/2'],
+    'b'
 );
+imgPregunt9 = document.createElement("img");
+imgPregunt9.src = "src/images/reto/ejercicios/eje9.jpeg";
 pregunta9 = new Preguntas(
-    'Aqui va la pregunta9',
-    ['Respuesta1', 'Respuesta2', 'Respuesta3', 'Respuesta4'],
-    'RespuestaCorrecta'
+    imgPregunt9,
+    ['a. -1/3', 'b. 1/3', 'c. -0.66', 'd. 0.66'],
+    'a'
 );
 pregunta10 = new Preguntas(
-    'Aqui va la pregunta10', //text
-    ['Respuesta1', 'Respuesta2', 'Respuesta3', 'Respuesta4'], //respuestas
-    'RespuestaCorrecta' //resp_correcta
+    '-0.3(4.2/10.5+(5.2)^2x√1.69-1.7', //text
+    ['a. -13.5', 'b. 9.915', 'c. 9.5', 'd. -9.915'], //respuestas
+    'd' //resp_correcta
 );
 
 //Array que almacena las preguntas
@@ -66,7 +74,7 @@ arrayPreguntas = [
 ];
 
 
-//Funcion que generara un array con 5 valores aleatorios
+//Funcion que desordena un array y retorna uno nuevo
 function indicesAzar(array){
     //Array que almacenara los indices de las preguntas
     indice = [];
@@ -81,23 +89,89 @@ function indicesAzar(array){
 
     return indice;
 }
-//Se eligen los primeros 5 valores de indice y se almacenan en el array indiceAzar
+
 indiceAzar = [];
-indiceAzar = indicesAzar(arrayPreguntas).slice(0, 5);
+//Se eligen los primeros 5 valores de indice y se almacenan en el array indiceAzar
+//indiceAzar = indicesAzar(arrayPreguntas).slice(0, 5);
+
+
+//Se eligen los primeros 10 valores de indice y se almacenan en el array indiceAzar
+indiceAzar = indicesAzar(arrayPreguntas).slice(0, 10);
 
 //Array para almacenar las preguntas elegidas al azar
 preguntasAzar = [];
-for (let i = 0; i < (arrayPreguntas.length/2); i++) {
+//for (let i = 0; i < (arrayPreguntas.length/2); i++) {
+//    preguntasAzar[i] = arrayPreguntas[indiceAzar[i]];
+//}
+for (let i = 0; i < arrayPreguntas.length; i++) {
     preguntasAzar[i] = arrayPreguntas[indiceAzar[i]];
 }
 
 
-document.getElementById('pregunta1').innerHTML = preguntasAzar[0].text;
-document.getElementById('resp1Preg1').innerHTML = preguntasAzar[0].respuestas[0];
-document.getElementById('resp2Preg1').innerHTML = preguntasAzar[0].respuestas[1];
-document.getElementById('resp3Preg1').innerHTML = preguntasAzar[0].respuestas[2];
-document.getElementById('resp4Preg1').innerHTML = preguntasAzar[0].respuestas[3];
+function escribirHTML(){
+    //Pregunta 1
+    document.getElementById('pregunta1').innerHTML = preguntasAzar[0].text;
+    document.getElementById('resp1Preg1').innerHTML = preguntasAzar[0].respuestas[0];
+    document.getElementById('resp2Preg1').innerHTML = preguntasAzar[0].respuestas[1];
+    document.getElementById('resp3Preg1').innerHTML = preguntasAzar[0].respuestas[2];
+    document.getElementById('resp4Preg1').innerHTML = preguntasAzar[0].respuestas[3];
+    //Pregunta 2
+    document.getElementById('pregunta2').innerHTML = preguntasAzar[1].text;
+    document.getElementById('resp1Preg2').innerHTML = preguntasAzar[1].respuestas[0];
+    document.getElementById('resp2Preg2').innerHTML = preguntasAzar[1].respuestas[1];
+    document.getElementById('resp3Preg2').innerHTML = preguntasAzar[1].respuestas[2];
+    document.getElementById('resp4Preg2').innerHTML = preguntasAzar[1].respuestas[3];
+    //Pregunta 3
+    document.getElementById('pregunta3').innerHTML = preguntasAzar[2].text;
+    document.getElementById('resp1Preg3').innerHTML = preguntasAzar[2].respuestas[0];
+    document.getElementById('resp2Preg3').innerHTML = preguntasAzar[2].respuestas[1];
+    document.getElementById('resp3Preg3').innerHTML = preguntasAzar[2].respuestas[2];
+    document.getElementById('resp4Preg3').innerHTML = preguntasAzar[2].respuestas[3];
+    //Pregunta 4
+    document.getElementById('pregunta4').innerHTML = preguntasAzar[3].text;
+    document.getElementById('resp1Preg4').innerHTML = preguntasAzar[3].respuestas[0];
+    document.getElementById('resp2Preg4').innerHTML = preguntasAzar[3].respuestas[1];
+    document.getElementById('resp3Preg4').innerHTML = preguntasAzar[3].respuestas[2];
+    document.getElementById('resp4Preg4').innerHTML = preguntasAzar[3].respuestas[3];
+    //Pregunta 5
+    document.getElementById('pregunta5').innerHTML = preguntasAzar[4].text;
+    document.getElementById('resp1Preg5').innerHTML = preguntasAzar[4].respuestas[0];
+    document.getElementById('resp2Preg5').innerHTML = preguntasAzar[4].respuestas[1];
+    document.getElementById('resp3Preg5').innerHTML = preguntasAzar[4].respuestas[2];
+    document.getElementById('resp4Preg5').innerHTML = preguntasAzar[4].respuestas[3];
+    //Pregunta 6
+    document.getElementById('pregunta6').innerHTML = preguntasAzar[5].text;
+    document.getElementById('resp1Preg6').innerHTML = preguntasAzar[5].respuestas[0];
+    document.getElementById('resp2Preg6').innerHTML = preguntasAzar[5].respuestas[1];
+    document.getElementById('resp3Preg6').innerHTML = preguntasAzar[5].respuestas[2];
+    document.getElementById('resp4Preg6').innerHTML = preguntasAzar[5].respuestas[3];
+    //Pregunta 7
+    document.getElementById('pregunta7').innerHTML = preguntasAzar[6].text;
+    document.getElementById('resp1Preg7').innerHTML = preguntasAzar[6].respuestas[0];
+    document.getElementById('resp2Preg7').innerHTML = preguntasAzar[6].respuestas[1];
+    document.getElementById('resp3Preg7').innerHTML = preguntasAzar[6].respuestas[2];
+    document.getElementById('resp4Preg7').innerHTML = preguntasAzar[6].respuestas[3];
+    //Pregunta 8
+    document.getElementById('pregunta8').innerHTML = preguntasAzar[7].text;
+    document.getElementById('resp1Preg8').innerHTML = preguntasAzar[7].respuestas[0];
+    document.getElementById('resp2Preg8').innerHTML = preguntasAzar[7].respuestas[1];
+    document.getElementById('resp3Preg8').innerHTML = preguntasAzar[7].respuestas[2];
+    document.getElementById('resp4Preg8').innerHTML = preguntasAzar[7].respuestas[3];
+    //Pregunta 9
+    document.getElementById('pregunta9').innerHTML = preguntasAzar[8].text;
+    document.getElementById('resp1Preg9').innerHTML = preguntasAzar[8].respuestas[0];
+    document.getElementById('resp2Preg9').innerHTML = preguntasAzar[8].respuestas[1];
+    document.getElementById('resp3Preg9').innerHTML = preguntasAzar[8].respuestas[2];
+    document.getElementById('resp4Preg9').innerHTML = preguntasAzar[8].respuestas[3];
+    //Pregunta 10
+    document.getElementById('pregunta10').innerHTML = preguntasAzar[9].text;
+    document.getElementById('resp1Preg10').innerHTML = preguntasAzar[9].respuestas[0];
+    document.getElementById('resp2Preg10').innerHTML = preguntasAzar[9].respuestas[1];
+    document.getElementById('resp3Preg10').innerHTML = preguntasAzar[9].respuestas[2];
+    document.getElementById('resp4Preg10').innerHTML = preguntasAzar[9].respuestas[3];
+}
 
+escribirHTML();
 
 //for (let index = 0; index < (arrayPreguntas.length/2); index++) {
 //    console.log(preguntasAzar[index].text);
