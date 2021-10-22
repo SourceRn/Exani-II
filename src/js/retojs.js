c85e047bbab1be40f3166680c84478eeab42bfff
SourceRn <ramnievesg@gmail.com>
2021-10-21 19:13:51 -0500

    Se han añadido 10 preguntas mas

--- src/js/retojs.js
+++ src/js/retojs.js
@@ -31,37 +31,94 @@ pregunta5 = new Preguntas(
 //imgPregunt6 = document.createElement("img");
 //imgPregunt6.src = "src/images/reto/ejercicios/eje6.PNG";
 pregunta6 = new Preguntas(
-    '-5{[(-3*2)/3]+1}+2-[-7(7-2)+1]', ['a. 13', 'b. 10', 'c. -9', 'd. 9'],
+    '-5{[(-3*2)/3]+1}+2-[-7(7-2)+1] =', ['a. 13', 'b. 10', 'c. -9', 'd. 9'],
     'c'
 );
 //imgPregunt7 = document.createElement("img");
 //imgPregunt7.src = "src/images/reto/ejercicios/eje7.jpeg";
 pregunta7 = new Preguntas(
-    '1/2(3/4 + 1/8)', ['a. 7/14', 'b. 5/16', 'c. 6/14', 'd. 7/16'],
+    '1/2(3/4 + 1/8) =', ['a. 7/14', 'b. 5/16', 'c. 6/14', 'd. 7/16'],
     'd'
 );
 //imgPregunt8 = document.createElement("img");
 //imgPregunt8.src = "src/images/reto/ejercicios/eje8.jpeg";
 pregunta8 = new Preguntas(
-    '(3/2 + 1/4) / (5/6 - 1/3)', ['a. 2/7', 'b. 3.5', 'c. -1.8', 'd. -7/2'],
+    '(3/2 + 1/4) / (5/6 - 1/3) =', ['a. 2/7', 'b. 3.5', 'c. -1.8', 'd. -7/2'],
     'b'
 );
 //imgPregunt9 = document.createElement("img");
 //imgPregunt9.src = "src/images/reto/ejercicios/eje9.jpeg";
 pregunta9 = new Preguntas(
-    '(-1 + 3/4 - 1/3) / (2 - 1/4)', ['a. -1/3', 'b. 1/3', 'c. -0.66', 'd. 0.66'],
+    '(-1 + 3/4 - 1/3) / (2 - 1/4) =', ['a. -1/3', 'b. 1/3', 'c. -0.66', 'd. 0.66'],
     'a'
 );
 pregunta10 = new Preguntas(
-    '-0.3(4.2/10.5+(5.2)^2x√1.69-1.7', //text
+    '-0.3(4.2/10.5+(5.2)^2x√1.69-1.7 =', //text
     ['a. -13.5', 'b. 9.915', 'c. 9.5', 'd. -9.915'], //respuestas
+    'd' //resp_correcta
+);
+pregunta11 = new Preguntas(
+    'En la fiesta, Aisha y sus amigos se comieron 2 1/2 pizzas. Despues de la fiesta, sobraba 1/8 de pizza.' +
+    '\n¿Cuantas Pizzas habia al inicio de la fiesta?', //text
+    ['a. 29/8', 'b. 27/8', 'c. 29/7', 'd. 24/8'], //respuestas
+    'a' //resp_correcta
+);
+pregunta12 = new Preguntas(
+    'El primer miembro de un equipo en una carrera de 3 relevos debe correr 2 1/4 vueltas, el segundo debe correr 1 1/2 vueltas y el tercero debe correr 3 5/8 vueltas.' +
+    '\n¿Cuantas vueltas en total debe correr cada equipo?', //text
+    ['a. 6 1/2 vueltas', 'b. 7 3/8', 'c. 8 4/5', 'd. 7 1/4'], //respuestas
+    'b' //resp_correcta
+);
+pregunta13 = new Preguntas(
+    'Priya camino 1 1/2 dias en una excursion. Estuvo perdida 5/6 de ese tiempo' +
+    '\n¿Que fraccion de un dia estuvo perdida Priya?', //text
+    ['a. 8/4 dias', 'b. 1 1/2', 'c. 1 1/4', 'd. 1 4/5'], //respuestas
+    'c' //resp_correcta
+);
+pregunta14 = new Preguntas(
+    'La estatura del hermano de Junior es 1 1/2 metros. La estatura de Junior es 1 1/5 de la de su hermano' +
+    '\n¿Cuanto mide Junior?', //text
+    ['a. 8/5 de metro', 'b. 3/5 de metro', 'c. 1 1/5 de metro', 'd. 4/5'], //respuestas
+    'd' //resp_correcta
+);
+pregunta15 = new Preguntas(
+    'Stephen utiliza 2/25 kilogramos de tofu en cada porcion de su famoso platillo de tofu. Tiene 1 1/10 kilogramos de tofu' +
+    '\n¿Cuantas porciones puede hacer Stephen?', //text
+    ['a. 55/4 porciones', 'b. 50/8 porciones', 'c. 55/2', 'd. 54/4'], //respuestas
+    'a' //resp_correcta
+);
+pregunta16 = new Preguntas(
+    '(3-5)^2 + 4(3-1) - 3(1+1)^2(2) + 2(3)(2) =', //text
+    ['a. -16', 'b. 0', 'c. 48', 'd. -19'], //respuestas
+    'b' //resp_correcta
+);
+pregunta17 = new Preguntas(
+    '3(3 - 1) + 3[1 + 2(2)] - 2(4 - 1)(2 + 1) =', //text
+    ['a. -3', 'b. 9', 'c. 3', 'd. 39'], //respuestas
+    'c' //resp_correcta
+);
+pregunta18 = new Preguntas(
+    '3√19-3 + 4(3 - 1)(2)^2 - 3(2 + 1)^2 - 2[2 + 3(2 + 1)]  =', //text
+    ['a. 5', 'b. -5', 'c. 0', 'd. -10'], //respuestas
+    'b' //resp_correcta
+);
+pregunta19 = new Preguntas(
+    '2(2 + 1)(3 - 1)^2 - 4(4 - 3)(3 + 1) =', //text
+    ['a. 8', 'b. -40', 'c. 40', 'd. -8'], //respuestas
+    'a' //resp_correcta
+);
+pregunta20 = new Preguntas(
+    '7(3 - 1) - 3[1 - 2(2)] - 2(4 + 1)(2 + 1) =', //text
+    ['a. -25', 'b. 25', 'c. 7', 'd. -7'], //respuestas
     'd' //resp_correcta
 );
 
 //Array que almacena las preguntas
 arrayPreguntas = [
     pregunta1, pregunta2, pregunta3, pregunta4, pregunta5,
-    pregunta6, pregunta7, pregunta8, pregunta9, pregunta10
+    pregunta6, pregunta7, pregunta8, pregunta9, pregunta10,
+    pregunta11, pregunta12, pregunta13, pregunta14, pregunta15,
+    pregunta16, pregunta17, pregunta18, pregunta19, pregunta20
 ];
 
 
@@ -85,7 +142,6 @@ indiceAzar = [];
 //Se eligen los primeros 5 valores de indice y se almacenan en el array indiceAzar
 //indiceAzar = indicesAzar(arrayPreguntas).slice(0, 5);
 
-
 //Se eligen los primeros 10 valores de indice y se almacenan en el array indiceAzar
 indiceAzar = indicesAzar(arrayPreguntas).slice(0, 10);
 
@@ -94,10 +150,9 @@ preguntasAzar = [];
 //for (let i = 0; i < (arrayPreguntas.length/2); i++) {
 //    preguntasAzar[i] = arrayPreguntas[indiceAzar[i]];
 //}
-for (let i = 0; i < arrayPreguntas.length; i++) {
+for (let i = 0; i < (arrayPreguntas.length - 10); i++) {
     preguntasAzar[i] = arrayPreguntas[indiceAzar[i]];
 }
-
 
 function verificarRespuestas() {
     preguntasTotales = preguntasAzar.length;
@@ -116,7 +171,7 @@ function verificarRespuestas() {
 
     for (let i = 1; i <= preguntasAzar.length; i++) {
         if (formularioReto["eje" + i].value === null || formularioReto["eje" + i].value === "") {
-            resultado.innerHTML = '<h3>Responde Todas las Preguntas: <span>Pregunta ' + 1 + ' Porfavor</span>';
+            resultado.innerHTML = '<h3>Responde Todas las Preguntas: <span> Revisa desde la Pregunta ' + i + ' Porfavor</span>';
             return false;
         } else {
             if (formularioReto["eje" + i].value === respuestas[i - 1]) {
